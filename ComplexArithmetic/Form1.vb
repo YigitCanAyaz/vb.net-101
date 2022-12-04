9532b60a7accc887eefa30501368e7c49a8166ee
Yiğit Can Ayaz <54958108+YigitCanAyaz@users.noreply.github.com>
2022-12-04 20:14:30 +0300

    Complex Arithmetic Example

---
+++ ComplexArithmetic/Form1.vb
@@ -0,0 +1,22 @@
+﻿Public Class Form1
+    Private Sub btnCalculate_Click(sender As Object, e As EventArgs) Handles btnCalculate.Click
+
+        Dim decPrice As Decimal
+        Dim iQuantity As Integer
+        Dim decDiscount As Decimal
+        Dim decTotalCost As Decimal
+        Dim decPostage As Decimal
+
+        decPrice = 5
+        iQuantity = 10
+        decDiscount = 2
+        decPostage = 3
+
+        decTotalCost = ((decPrice - decDiscount) * iQuantity) + decPostage
+
+        'BO(DM)(AS) => o stands for power of number, b => brackets
+
+        MsgBox(decTotalCost)
+
+    End Sub
+End Class

1df1bf373b2375840fe6466c759ca727cc59dcef
Yiğit Can Ayaz <54958108+YigitCanAyaz@users.noreply.github.com>
2022-12-04 19:32:53 +0300

    Input with Windows Forms Example

---
+++ CollectUserInput/Form1.vb
@@ -0,0 +1,30 @@
+﻿Public Class Form1
+    Private Sub btnGo_Click(sender As Object, e As EventArgs) Handles btnGo.Click
+        'Dim stFirstName As String
+
+        'stFirstName = InputBox("Please enter your first name")
+
+        'MsgBox("hello " & stFirstName)
+
+        Dim stFirstName As String
+        Dim stLastName As String
+        Dim stGender As String
+        Dim stOccupation As String
+
+        stFirstName = txtFirstName.Text
+        stLastName = txtLastName.Text
+        stGender = txtGender.Text
+
+        stOccupation = lstOccupation.SelectedItem
+
+        MsgBox("hello " & stFirstName & " " & stLastName & " and you are a " & stGender & " " & stOccupation)
+
+
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        lstOccupation.Items.Add("Writer")
+        lstOccupation.Items.Add("Actor")
+        lstOccupation.Items.Add("Painter")
+    End Sub
+End Class

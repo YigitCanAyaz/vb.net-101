6b808773e8cf516df21e9db7127e5fe1c330bc41
Yiğit Can Ayaz <54958108+YigitCanAyaz@users.noreply.github.com>
2022-12-04 20:06:24 +0300

    Arithmetic Operations Example

---
+++ Arithmetic/Form1.vb
@@ -0,0 +1,42 @@
+﻿Public Class Form1
+    Private Sub btnCalculate_Click(sender As Object, e As EventArgs) Handles btnCalculate.Click
+
+        Dim dblNumber1 As Double
+        Dim dblNumber2 As Double
+
+        Dim dblResult As Double
+
+        dblNumber1 = txtNumber1.Text
+        dblNumber2 = txtNumber2.Text
+
+        dblResult = dblNumber1 + dblNumber2
+
+        MsgBox(dblResult)
+
+        dblResult = dblNumber1 - dblNumber2
+
+        MsgBox(dblResult)
+
+        dblResult = dblNumber1 * dblNumber2
+
+        MsgBox(dblResult)
+
+        dblResult = dblNumber1 / dblNumber2
+
+        MsgBox(dblResult)
+
+        dblResult = dblNumber1 ^ dblNumber2 'power
+
+        MsgBox(dblResult)
+
+        dblResult = dblNumber1 \ dblNumber2 '(will not give the decimal)
+
+        MsgBox(dblResult)
+
+        dblResult = dblNumber1 Mod dblNumber2
+
+        MsgBox(dblResult)
+
+
+    End Sub
+End Class

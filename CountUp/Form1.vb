0c3da0df53f426951bf81a9e63978e06326dd842
Yiğit Can Ayaz <54958108+YigitCanAyaz@users.noreply.github.com>
2022-12-05 10:31:42 +0300

    Practice for next loops & if blocks

---
+++ CountUp/Form1.vb
@@ -0,0 +1,27 @@
+﻿Public Class Form1
+    Private Sub btnCount_Click(sender As Object, e As EventArgs) Handles btnCount.Click
+
+        Dim iRange As Integer
+        iRange = CInt(InputBox("What number do you want to count up to"))
+
+        Dim sType As String
+        sType = InputBox("Dou you want odd numbers or even numbers?")
+
+        Dim iCount As Integer
+
+        If sType.ToUpper() = "EVEN" Then
+            iCount = 0
+        ElseIf sType.ToUpper() = "ODD" Then
+            iCount = 1
+        Else
+            MsgBox("Please write even or odd!")
+            Exit Sub
+        End If
+
+        For i = iCount To iRange Step 2
+            MsgBox(i)
+        Next
+
+
+    End Sub
+End Class

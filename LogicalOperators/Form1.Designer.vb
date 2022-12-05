9f8025be904c091be9fcf6ec6adbd0c254f22b3b
Yiğit Can Ayaz <54958108+YigitCanAyaz@users.noreply.github.com>
2022-12-05 09:33:26 +0300

    Logical and relational operators example 1

---
+++ LogicalOperators/Form1.Designer.vb
@@ -0,0 +1,73 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.lblExamScore = New System.Windows.Forms.Label()
+        Me.txtExamScore = New System.Windows.Forms.TextBox()
+        Me.btnGrade = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'lblExamScore
+        '
+        Me.lblExamScore.AutoSize = True
+        Me.lblExamScore.Location = New System.Drawing.Point(98, 92)
+        Me.lblExamScore.Name = "lblExamScore"
+        Me.lblExamScore.Size = New System.Drawing.Size(138, 13)
+        Me.lblExamScore.TabIndex = 0
+        Me.lblExamScore.Text = "Enter the examination score"
+        '
+        'txtExamScore
+        '
+        Me.txtExamScore.Location = New System.Drawing.Point(288, 87)
+        Me.txtExamScore.Name = "txtExamScore"
+        Me.txtExamScore.Size = New System.Drawing.Size(74, 20)
+        Me.txtExamScore.TabIndex = 1
+        '
+        'btnGrade
+        '
+        Me.btnGrade.Location = New System.Drawing.Point(189, 184)
+        Me.btnGrade.Name = "btnGrade"
+        Me.btnGrade.Size = New System.Drawing.Size(119, 78)
+        Me.btnGrade.TabIndex = 2
+        Me.btnGrade.Text = "Get Grade"
+        Me.btnGrade.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.btnGrade)
+        Me.Controls.Add(Me.txtExamScore)
+        Me.Controls.Add(Me.lblExamScore)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents lblExamScore As Label
+    Friend WithEvents txtExamScore As TextBox
+    Friend WithEvents btnGrade As Button
+End Class
---
+++ LogicalOperators/Form1.vb
@@ -0,0 +1,28 @@
+﻿Public Class Form1
+    Private Sub btnGrade_Click(sender As Object, e As EventArgs) Handles btnGrade.Click
+
+        Dim iScore As Integer
+
+        If IsNumeric(txtExamScore.Text) = True Then
+            iScore = CInt(txtExamScore.Text) 'convert integer manually
+        Else
+            MsgBox("You must enter a number")
+            Exit Sub
+        End If
+
+        If iScore < 0 Or iScore > 100 Then
+            MsgBox("That is not a valid score. Enter a number between 0 and 100!")
+            'Exit Sub 'forces program to stop => return
+        ElseIf iScore >= 50 Then
+            MsgBox("pass")
+        ElseIf iScore < 50 Then
+            MsgBox("fail")
+        End If
+
+        ' > < = (these are relational)
+        ' Or And (these are logical)
+
+        MsgBox("all done")
+
+    End Sub
+End Class

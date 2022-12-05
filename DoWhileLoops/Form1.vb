d3ea6c9e6a9730032e6aaf37517564f6e61bc897
Yiğit Can Ayaz <54958108+YigitCanAyaz@users.noreply.github.com>
2022-12-05 11:14:22 +0300

    Do while example

---
+++ DoWhileLoops/Form1.vb
@@ -0,0 +1,56 @@
+﻿Public Class Form1
+    Private Sub btnGo_Click(sender As Object, e As EventArgs) Handles btnGo.Click
+
+        Dim iCount As Integer
+        'For iCount = 1 To 5
+        '    MsgBox("Hello " & iCount)
+        'Next
+
+        'Do While iCount < 5
+        '    iCount = iCount + 1
+        '    MsgBox("Hello " & iCount)
+        'Loop
+
+        'Do Until iCount = 5
+        '    iCount = iCount + 1
+        '    MsgBox("Hello " & iCount)
+        'Loop
+
+        ' If condition is below, the while loop atleast will execute one time
+
+        'iCount = 5
+        'Do
+        '    MsgBox("Hello " & iCount)
+        'Loop Until iCount = 5
+
+        iCount = 0
+        Do While iCount < 5
+            iCount = iCount + 1
+            MsgBox("Hello " & iCount)
+        Loop
+
+        iCount = 10
+        Do 
+            iCount = iCount + 1
+            MsgBox("Hello " & iCount)
+        Loop While iCount < 5
+
+
+        iCount = 0
+        Do Until iCount = 5
+            iCount = iCount + 1
+            MsgBox("Hello " & iCount)
+        Loop
+
+        
+        iCount = 0
+        Do
+            iCount = iCount + 1
+            MsgBox("Hello " & iCount)
+        Loop Until iCount = 5
+
+
+        MsgBox("all done")
+
+    End Sub
+End Class
---
+++ DoWhileLoops/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("DoWhileLoops")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("")>
+<Assembly: AssemblyProduct("DoWhileLoops")>
+<Assembly: AssemblyCopyright("Copyright ©  2022")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("d015c06f-1b43-4511-938b-d26321fda00a")>
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")>
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>

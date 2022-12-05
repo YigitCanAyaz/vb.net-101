5f14e4dac4040a67114e732c39eeb3801f3e5abf
Yiğit Can Ayaz <54958108+YigitCanAyaz@users.noreply.github.com>
2022-12-05 10:07:41 +0300

    Select case example

---
+++ SelectCase/Form1.vb
@@ -0,0 +1,27 @@
+﻿Public Class Form1
+    Private Sub btnCheck_Click(sender As Object, e As EventArgs) Handles btnCheck.Click
+
+        Dim iTemperature As Integer
+        iTemperature = CInt(txtTemperature.Text)
+
+        Dim iWindSpeed As Integer
+        iWindSpeed = 30
+
+        Select Case iTemperature
+            Case Is = 0
+                MsgBox("Freezing")
+            Case Is < 0
+                MsgBox("Sub zero")
+                If iWindSpeed > 20 Then
+                    MsgBox("that will feel really cold")
+                End If
+            Case 1 To 10
+                MsgBox("Cold")
+            Case 11 To 20
+                MsgBox("Warm")
+            Case Else
+                MsgBox("Hot")
+        End Select
+
+    End Sub
+End Class

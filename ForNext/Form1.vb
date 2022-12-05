1569dc3413c65bbf71b98f036e127458219481d0
Yiğit Can Ayaz <54958108+YigitCanAyaz@users.noreply.github.com>
2022-12-05 10:17:10 +0300

    For next example

---
+++ ForNext/Form1.vb
@@ -0,0 +1,18 @@
+﻿Public Class Form1
+    Private Sub btnGo_Click(sender As Object, e As EventArgs) Handles btnGo.Click
+
+        Dim iCount As Integer
+        Dim stOut As String
+
+        For iCount = 50 To 0 Step -5
+            'Beep()
+            'Threading.Thread.Sleep(3000)
+            'MsgBox(iCount)
+            stOut = stOut & iCount & vbNewLine
+        Next
+
+        MsgBox(stOut)
+
+
+    End Sub
+End Class

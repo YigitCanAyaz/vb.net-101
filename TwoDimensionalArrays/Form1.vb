3860b125579bd80daf597b85d4e25c5936e83da1
Yiğit Can Ayaz <54958108+YigitCanAyaz@users.noreply.github.com>
2022-12-05 13:36:20 +0300

    Two dimensional arrays example

---
+++ TwoDimensionalArrays/Form1.vb
@@ -0,0 +1,23 @@
+﻿Public Class Form1
+    Private Sub btnStart_Click(sender As Object, e As EventArgs) Handles btnStart.Click
+
+        Dim astPeople(4, 5) As String ' row x column
+
+        astPeople(0, 0) = "Barack"
+        astPeople(1, 0) = "Obama"
+        astPeople(2, 0) = "Male"
+        astPeople(3, 0) = "American"
+        astPeople(4, 0) = "President"
+
+        astPeople(0, 1) = "Jacinda"
+        astPeople(1, 1) = "Ardern"
+        astPeople(2, 1) = "Female"
+        astPeople(3, 1) = "New Zealand"
+        astPeople(4, 1) = "Prime Minister"
+
+        For x = 0 To 4
+            MsgBox(astPeople(x, 0))
+        Next
+
+    End Sub
+End Class
